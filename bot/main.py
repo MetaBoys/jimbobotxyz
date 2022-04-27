90004b207e62735d110306ae949019a502cc1ca1
Jon <98303829+MetaBoys@users.noreply.github.com>
2022-04-27 21:34:11 +0800

    update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,16 +1,120 @@
+# bot.py
 import os
-from discord.ext import commands
+import discord
+from dotenv import load_dotenv
 
-bot = commands.Bot(command_prefix="!")
-TOKEN = os.getenv("DISCORD_TOKEN")
+load_dotenv()
+TOKEN = 'OTY3Nzg4NzYxNzk2NzA2MzA0.YmVZhQ.vNvOdXhxIRv6VEsYxR9ZrHDFido'
 
-@bot.event
+client = discord.Client(intents=discord.Intents.all())
+
+
+@client.event
 async def on_ready():
-    print(f"Logged in as {bot.user.name}({bot.user.id})")
+  print(f'{client.user} has connected to Discord!')
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send("pong")
 
-if __name__ == "__main__":
-    bot.run(TOKEN)
+@client.event
+async def on_member_join(member):
+    await member.edit(nick='Jimbo')
+
+
+
+@client.event
+async def on_message(message):
+
+  isMessageJimbo = message.content.replace(" ","")
+  isMessageJimbo = isMessageJimbo.lower()
+  
+  index = 0
+  if message.channel.name == 'jimbo':
+    messageDelete = False
+  
+    if (len(isMessageJimbo) < 5) or (len(isMessageJimbo) %5 !=0):
+      messageDelete = True
+
+
+    while index + 1 < len(isMessageJimbo):
+      
+      if isMessageJimbo[index] != "j":
+        messageDelete = True
+        break
+      index += 1
+      
+      if isMessageJimbo[index] != "i":
+        messageDelete = True
+        break
+      index += 1
+        
+      if isMessageJimbo[index] != "m":
+        messageDelete = True
+        break
+      index += 1
+        
+      if isMessageJimbo[index] != "b":
+        messageDelete = True
+        break
+      index += 1
+        
+      if isMessageJimbo[index] != "o":
+        messageDelete = True
+        break
+      index += 1
+
+    if messageDelete:
+      await message.delete()
+      
+  
+  if message.channel.name == 'shill':
+    angry = False
+
+    if(message.author.bot): return
+    if (len(isMessageJimbo) < 5) or (len(isMessageJimbo) %5 !=0):
+      angry = True
+
+
+    while index + 1 < len(isMessageJimbo):
+      if angry:
+        break
+      
+      if isMessageJimbo[index] != "j":
+        angry = True
+
+        break
+      index += 1
+      
+      if isMessageJimbo[index] != "i":
+        angry = True
+      
+        break
+      index += 1
+        
+      if isMessageJimbo[index] != "m":
+        angry = True
+        break
+      index += 1
+        
+      if isMessageJimbo[index] != "b":
+        angry = True
+        break
+      index += 1
+        
+      if isMessageJimbo[index] != "o":
+        angry = True
+     
+        break
+      index += 1
+
+      
+    if angry:
+      if(message.author.bot): return
+      await message.channel.send(file= discord.File("angryJimbo.jpg"))
+        
+
+  if message.author == client.user:
+        return
+
+
+
+client.run(TOKEN)
+
